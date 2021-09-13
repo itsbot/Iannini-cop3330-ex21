820db0aac50a9a326c1265007aa15f529ea3e7ca
itsbot <patrickiannini@knights.ucf.edu>
2021-09-12 23:15:24 -0400

    Completed Exercise

--- src/main/java/Exercise21/Exercise21.java
+++ src/main/java/Exercise21/Exercise21.java
@@ -3,9 +3,23 @@
  *  Copyright 2021 Patrick Iannini
  */
 package Exercise21;
+import java.time.Month;
+import java.util.Scanner;
 
 public class Exercise21 {
     public static void main(String[] args) {
-        
+        System.out.println("Please enter the number of the month: ");
+        Scanner sc = new Scanner(System.in);
+        while (!sc.hasNextInt()) {
+            System.out.println("Please enter a valid month");
+            sc.next();
+        }
+        int num = sc.nextInt();
+        while (num<1||num>12) {
+            System.out.println("Please enter a valid month");
+            num = sc.nextInt();
+        }
+        Month month = Month.of(num);
+        System.out.println("The name of the month is "+month);
     }
 }
